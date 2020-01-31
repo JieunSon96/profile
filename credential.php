e9ed955f0ff22bc7eb772341b6b0ea361a09913d
Jieun <plmn855000@gmail.com>
2020-01-31 17:52:18 +0900

    skill images are added

--- credential.php
+++ credential.php
@@ -1,4 +1,11 @@
 <?php
-$gmail_id="*";
-$gmail_pw="*";
+ // define('EMAIL','plmn855000@gmail.com')
+ // define('PASS','tpfk2407917!')
+// $EMAIL='plmn855000@gmail.com';
+// $PASS='tpfk2407917!';
+//global $gmail_id;
+$gmail_id="plmn855000@gmail.com";
+
+//global $gmail_pw;
+$gmail_pw="tpfk2407917!";
 ?>
